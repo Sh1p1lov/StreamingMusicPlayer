f0f53e043a03909cff61c6aa80efb99426737aac
sh1p1lov <sergey.shipilov1999@mail.ru>
2022-07-17 20:53:13 +0300

    view binding added

--- app/src/main/java/com/sh1p1lov/streamingmusicplayer/MainActivity.kt
+++ app/src/main/java/com/sh1p1lov/streamingmusicplayer/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sh1p1lov.streamingmusicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sh1p1lov.streamingmusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
