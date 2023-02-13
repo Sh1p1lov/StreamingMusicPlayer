9b1e0b6da9a7da310ee98536541def33ae944aab
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-02-13 21:36:09 +0300

    changed light theme

--- app/src/main/java/com/sh1p1lov/streamingmusicplayer/app/App.kt
+++ app/src/main/java/com/sh1p1lov/streamingmusicplayer/app/App.kt
@@ -9,6 +9,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
+// App
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
