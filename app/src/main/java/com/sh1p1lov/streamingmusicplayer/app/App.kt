c505da6daf490e161add400c7478f21575259432
MaximT89 <maximah1989@yandex.ru>
2023-02-13 21:45:16 +0300

    created hotfix

--- app/src/main/java/com/sh1p1lov/streamingmusicplayer/app/App.kt
+++ app/src/main/java/com/sh1p1lov/streamingmusicplayer/app/App.kt
@@ -10,6 +10,7 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 // App
+// Created hotfix
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
