7d73be7efd63d1e350672bb67029614804424e40
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-02-14 02:12:27 +0300

    Revert "changed name Service"
    This reverts commit a00f8637a8cad596b66d57717d4266b45f884494.

--- data/src/main/java/com/sh1p1lov/streamingmusicplayer/data/api/MusicServiceNew.kt
+++ data/src/main/java/com/sh1p1lov/streamingmusicplayer/data/api/MusicServiceNew.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface MusicServiceNewAfterCommit {
+interface MusicServiceNew {
 
     @GET("${MusicService.API_VERSION}/tracks/?client_id=${MusicService.CLIENT_ID}&format=json&include=musicinfo+stats")
     suspend fun getTracks(
